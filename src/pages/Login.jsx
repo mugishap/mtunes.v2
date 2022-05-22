86474d81aa29cee143fbe9bde9d16634a1f08aa8
mugishap <precieuxmugisha@gmail.com>
2022-05-22 16:37:09 +0200

    Added material UI

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,84 +1,75 @@
-import { React, useState } from 'react'
-import Signup from './Signup'
+import { Button, TextField } from "@mui/material";
+import { React, useState } from "react";
+import Signup from "./Signup";
+
 
 function Login() {
-  const [login, setLogin] = useState(true)
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [loader, setLoader] = useState(false)
+  const [login, setLogin] = useState(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loader, setLoader] = useState(false);
 
-  const handleChangeForm = e => {
-    setLogin(false)
-  }
-  const handleFormSubmit = async e => {
-    e.preventDefault()
-    setLoader(true)
-    const api = await fetch('https://mtunesv2.herokuapp.com/user/login', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      credentials: 'include',
+  const handleChangeForm = (e) => {
+    setLogin(false);
+  };
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    setLoader(true);
+    const api = await fetch("https://mtunesv2.herokuapp.com/user/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
       body: JSON.stringify({
-        email: (email),
-        password: (password)
-      })
-    })
-    const data = await api.json()
-    console.log(data)
+        email: email,
+        password: password,
+      }),
+    });
+    const data = await api.json();
+    console.log(data);
     if (data.message === "Allowed to continue") {
-      window.location.replace('/home')
-      localStorage.setItem('token', data.token)
+      window.location.replace("/home");
+      localStorage.setItem("mTunesToken", data.token);
+      localStorage.setItem("mTunesUserProfile", data.user);
+    } else {
+      window.alert("Either password or email is wrong try again please");
+      setLoader(false);
     }
-    else {
-      window.alert("Either password or email is wrong try again please")
-      setLoader(false)
-    }
-
-  }
+  };
   return (
     <>
-      {login ?
+      {login ? (
 
-        <div className="form h-screen w-full flex flex-col items-center">
-          <div className='m-auto w-1/3 flex flex-col shadow-2xl p-3 items-center'>
-            <div className="inNav ml-2 mb-3 logo w-full flex flex-col items-center justify-center">
-              <img src={require('./../images/logo1.png')} className='w-15' alt="" />
-              <div className='font-bold text-lg overflow-hidden'>Log in to mTunes</div>
-            </div>
-            <form className="text-m p-2 w-full h-4/5">
+        <div className="h-[90vh] w-screen flex justify-center items-center">
+          <div className="w-1/3 h-2/3  shadow-2xl shadow-black ">
+            <form className="w-full h-full flex flex-col items-center justify-center">
+              <TextField
+                required
+                id="outlined-basic"
+                label="Email"
+                variant="standard"
+                onChange={(e)=>{setEmail(e.target.value)}}
 
-              <div className="flex flex-row items-center justify-between m-2">
-                <label className='w-1/3 h-6 flex justify-left'>Email:</label>
-                <input type="email" name='email' className='p-1 rounded bg-gray-200 w-2/3 h-8' onChange={(e) => {
-                  setEmail(e.target.value)
-                }} />
-              </div>
-              <div className="flex flex-row items-center justify-between m-2">
-                <label className='w-1/3 h-6 flex justify-left'>Password:</label>
-                <input type="password" name='password' className='p-1 rounded bg-gray-200 w-2/3 h-8' onChange={(e) => {
-                  setPassword(e.target.value)
-                }} />
-              </div>
+                className="w-10/12 mb-32"
+              />
+              <TextField
+              onChange={(e)=>{setPassword(e.target.value)}}
+                required
+                id="outlined-basic"
+                label="Password"
+                variant="standard"
+                className="w-10/12"
+                type='password'
+              />
+                      <Button variant="contained" size="large" >Submit</Button>
 
-              <div className="flex flex-col items-center justify-center m-2">
-                {loader ?
-                  <>
-                    <p className='text-sm'>Please wait while you request is being processed</p>
-                    <img className='h-12' src={require('./../images/loader.gif')} alt="" />
-                  </>
-                  :
-                  <button type="submit" onClick={handleFormSubmit} className='shadow-2xl w-24 border-3 p-1 rounded hover:text-white hover:bg-orange-500 border'>Submit</button>
-
-                }
-              </div>
             </form>
-            <div className="flex flex-row items-center justify-center overflow-hidden">
-              <p>Don't have an account?</p>&nbsp;<p onClick={handleChangeForm} className='hover:text-orange-500 font-bold cursor-pointer'>Sign up</p>
-            </div>
           </div>
-        </div> : <Signup />
-      }
+        </div>
+      ) : (
+        <Signup />
+      )}
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- src/pages/check.js
+++ src/pages/check.js
@@ -1,29 +1,9 @@
-
 export async function checkForAccess() {
-    try {
-        let res = await fetch('https://mtunesv2.herokuapp.com/user/checkForAccess', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            credentials: 'include',
-            body: JSON.stringify({
-                token: (localStorage.getItem('token'))
-            })
-        })
-        res = await res.json()
-        if (res.message === "#NoTokenNoEntry") {
-            // window.alert('No token generated!!! Authentication gone wrong')
-            window.location.replace('/login')
-        }
-        else if (res.message === "#FailedToParseToken") {
-            // window.alert('Error in parsing token')
-            window.location.replace('/login')
-        }
-        else if (res.message === "#Success") {
-            localStorage.setItem('userInfo',JSON.stringify(res.userInfo.user))
-            // window.location.replace('/home')
-        }
-    } catch (e) {
-        console.log("Error here", e)
-    }
+  try {
+    localStorage.getItem("mTunesUserProfile")
+      ? console.log("Allowed to continue")
+      : window.location.replace("/login");
+  } catch (e) {
+    console.log("Error here", e);
+  }
 }
-
