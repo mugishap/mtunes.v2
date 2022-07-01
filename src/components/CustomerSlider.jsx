7269ab0529e73f27bccdfd94baf6af923178cc9a
mugishap <precieuxmugisha@gmail.com>
2022-07-01 11:35:15 +0200

    modifying customer slider

---
+++ src/components/CustomerSlider.jsx
@@ -0,0 +1,49 @@
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+function CustomerSlider() {
+  return (
+    <Carousel fade>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_181b90cf086%20text%20%7B%20fill%3A%23ffffff%3Bfont-weight%3Anormal%3Bfont-family%3Avar(--bs-font-sans-serif)%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_181b90cf086%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%2320232a%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22274.2734375%22%20y%3D%22218%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_181b90cf086%20text%20%7B%20fill%3A%23ffffff%3Bfont-weight%3Anormal%3Bfont-family%3Avar(--bs-font-sans-serif)%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_181b90cf086%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%2320232a%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22274.2734375%22%20y%3D%22218%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_181b90cf086%20text%20%7B%20fill%3A%23ffffff%3Bfont-weight%3Anormal%3Bfont-family%3Avar(--bs-font-sans-serif)%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_181b90cf086%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%2320232a%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22274.2734375%22%20y%3D%22218%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default CustomerSlider;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
-import {Swiper,SwiperSlide} from 'swiper/react'
-import "swiper/css";
-import "swiper/css/navigation";
+import CustomerSlider from '../components/CustomerSlider';
 
-import "./styles.css";
 
 function Landing() {
 
@@ -35,30 +32,30 @@ function Landing() {
                             Everything</span>
                     </div>
                     <div className='w-1/2 h-full flex items-center justify-center'>
-                        <img src={require('./../images/hero.png')} alt="" />
-                    </div>
+                        <img className='hover:rotate-12 rounded-md' src={require('./../images/hero.png')} alt="" />
+                    </  div>
                 </div>
                 <div className="benefits w-full">
                     <h1 className='mb-4 text-5xl font-bold whitespace-nowrap'>Our<p className="listening inline"> Classic Benefits</p><br></br>
                         for your music</h1>
                 </div>
                 <div className="flex items-center  justify-around w-11/12">
-                    <div className='w-1/3 m-4 h-[30vh] bg-gray-700 rounded-xl bordered flex flex-col items-center justify-center'>
+                    <div className='hover:scale-105 hover:-translate-y-4 shadow-black shadow-md w-1/3 m-4 h-[30vh] bg-gray-700 rounded-xl bordered flex flex-col items-center justify-center p-4'>
                         <div className='flex items-start justify-center rounded-full w-[55px] mb-8 h-[55px] bg-gray-800'><i className='bi-emoji-smile' style={{ 'font-size': '2rem', 'color': 'orange' }}></i></div>
                         <h1 className='text-white font-bold text-2xl mb-4'>Reduces stress
                         </h1>
                         <span className='text-xl text-white'>
                             Research has found that listening to music can relieve stress by triggering biochemical stress reducers.</span>
                     </div>
-                    <div className='w-1/3 m-4 h-[30vh] bg-gray-700 rounded-xl bordered flex flex-col items-center justify-center'>
+                    <div className='hover:scale-105 hover:-translate-y-4 shadow-black shadow-md w-1/3 m-4 h-[30vh] bg-gray-700 rounded-xl bordered flex flex-col items-center justify-center p-4'>
                         <div className='flex items-start justify-center rounded-full w-[55px] mb-8 h-[55px] bg-gray-800'><i className='bi-emoji-heart-eyes' style={{ 'font-size': '2rem', 'color': 'red' }}></i></div>
                         <h1 className='text-white font-bold text-2xl mb-4'>Heart healthy
                         </h1>
                         <span className='text-xl text-white'>
-                            Music can boost the brain’s production of the hormone dopamine.</span>
+                            Music can boost the brain&apos;s production of the hormone dopamine.</span>
 
                     </div>
-                    <div className='w-1/3 m-4 h-[30vh] bg-gray-700 rounded-xl bordered flex flex-col items-center justify-center'>
+                    <div className='hover:scale-105 hover:-translate-y-4 shadow-black shadow-md w-1/3 m-4 h-[30vh] bg-gray-700 rounded-xl bordered flex flex-col items-center justify-center p-4'>
                         <div className='flex items-start justify-center rounded-full w-[55px] mb-8 h-[55px] bg-gray-800'><i className='bi-emoji-sunglasses' style={{ 'font-size': '2rem', 'color': '#a213e5' }}></i></div>
                         <h1 className='text-white font-bold text-2xl mb-4'>Elevates mood</h1>
                         <span className='text-xl text-white'>
@@ -68,8 +65,10 @@ function Landing() {
                 </div>
                 <div className="customers mt-16 ">
                     <h1 className='w-full mb-4 text-4xl font-bold'>What <p className='inline listening'>Customers </p>
-                         say about us</h1>
+                        say about us</h1>
+                    <CustomerSlider />
                 </div>
+                <div className="flex items-center justify-center"></div>
             </div>
         </div>
     )
