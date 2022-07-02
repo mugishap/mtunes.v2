87d53c7f38ad79d6d7c9b6084b2346209e46b711
mugishap <precieuxmugisha@gmail.com>
2022-07-02 21:18:41 +0200

    remaining some few responsiveness

--- src/utils/downloader.js
+++ src/utils/downloader.js
@@ -1,6 +1,5 @@
-import { log } from 'console'
+// const fs = require('fs')
 
-const fs = require('fs')
 const ytdl = require('ytdl-core')
 const download = async(id) => {
     console.log(`Downloading ${id}`);
